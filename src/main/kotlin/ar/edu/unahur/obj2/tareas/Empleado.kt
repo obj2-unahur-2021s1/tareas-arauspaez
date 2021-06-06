6cf27b884f83572ead2c4628179d04c39991e68b
JulianPaez92 <jepaez92@gmail.com>
2021-06-06 15:54:09 -0300

    interface TipoTarea

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Empleado.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Empleado.kt
@@ -1,7 +1,5 @@
 package ar.edu.unahur.obj2.tareas
 
-abstract class TipoEmpleado(open val costoHoraTrabajo: Int){ }
+class Empleado(val costoHoraTrabajo: Int){}
 
-class Empleado(override val costoHoraTrabajo: Int) : TipoEmpleado(costoHoraTrabajo) { }
-
-class Responsable(override val costoHoraTrabajo: Int) : TipoEmpleado(costoHoraTrabajo) { }
+class Responsable(val costoHoraTrabajo: Int){}
--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,14 +1,31 @@
 package ar.edu.unahur.obj2.tareas
 
-open class Tarea(open val horasEstimadas: Int,open val responsable: TipoEmpleado){
-    var costoInfraestructura: Int = 0 // Tratar de que no tenga un inicializador
-    lateinit var empleados: MutableList<TipoEmpleado>
+import kotlin.properties.Delegates
 
-    fun horasNecesarias() = horasEstimadas / empleados.size
-    fun salarioEmpleado(empleado: TipoEmpleado) = empleado.costoHoraTrabajo * this.horasNecesarias()
-    fun salarioResponsable() = responsable.costoHoraTrabajo * horasEstimadas
-    fun salarioTotalEmpleados() = empleados.sumBy { salarioEmpleado(it) }
-    fun costoTarea() = this.salarioTotalEmpleados() + this.salarioResponsable() + costoInfraestructura
+interface TipoTarea{
+    fun horasNecesarias(): Int
+    fun salarioEmpleado(empleado: Empleado): Int
+    fun salarioResponsable(): Int
+    fun salarioTotalEmpleados(): Int
+    fun costoTarea(): Int
 }
 
-class TareaIntegracion(override val horasEstimadas: Int,override val responsable: TipoEmpleado): Tarea(horasEstimadas,responsable){ }
+class Tarea(val horasEstimadas: Int, val responsable: Responsable): TipoTarea{
+
+    val costoInfraestructura by Delegates.notNull<Int>()  // Tratar de que no tenga un inicializador
+    val empleados = mutableListOf<Empleado>()
+
+    override fun horasNecesarias() = horasEstimadas / empleados.size
+    override fun salarioEmpleado(empleado: Empleado) = empleado.costoHoraTrabajo * this.horasNecesarias()
+    override fun salarioResponsable() = responsable.costoHoraTrabajo * horasEstimadas
+    override fun salarioTotalEmpleados() = empleados.sumBy { salarioEmpleado(it) }
+    override fun costoTarea() = costoInfraestructura + this.salarioTotalEmpleados() + this.salarioResponsable()
+}
+
+class TareaIntegracion(val horasEstimadas: Int, val responsable: Responsable): TipoTarea{
+    override fun horasNecesarias() = 1
+    override fun salarioEmpleado(empleado: Empleado) = 1
+    override fun salarioResponsable() = 1
+    override fun salarioTotalEmpleados() = 1
+    override fun costoTarea() = 1
+}
