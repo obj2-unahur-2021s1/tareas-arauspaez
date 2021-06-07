b75a18724b1e7edb64cd5dabb05085642b0cdce4
JulianPaez92 <jepaez92@gmail.com>
2021-06-07 01:07:09 -0300

    Update: nominaEmpleados,falta testear

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -5,7 +5,7 @@ import kotlin.properties.Delegates
 interface TipoTarea{
     fun horasNecesarias(): Int
     fun costoTarea(): Int
-    fun salarioTotalEmpleados() : Int
+    fun nominaEmpleados(): List<Empleado>
 }
 
 class TareaSimple(val horasEstimadas: Int, val responsable: Responsable): TipoTarea{
@@ -15,8 +15,10 @@ class TareaSimple(val horasEstimadas: Int, val responsable: Responsable): TipoTa
     override fun horasNecesarias() = horasEstimadas / empleados.size
     fun salarioEmpleado(empleado: Empleado) = empleado.costoHoraTrabajo * this.horasNecesarias()
     fun salarioResponsable() = responsable.costoHoraTrabajo * horasEstimadas
-    override fun salarioTotalEmpleados() = empleados.sumBy { salarioEmpleado(it) }
+    fun salarioTotalEmpleados() = empleados.sumBy { salarioEmpleado(it) }
     override fun costoTarea() = costoInfraestructura + this.salarioTotalEmpleados() + this.salarioResponsable()
+
+    override fun nominaEmpleados(): List<Empleado> = empleados + responsable
 }
 
 class TareaIntegracion(val horasEstimadas: Int, val responsable: Responsable): TipoTarea{
@@ -24,12 +26,12 @@ class TareaIntegracion(val horasEstimadas: Int, val responsable: Responsable): T
 
     fun totalHorasSubtareas() = listaTareas.sumBy { it.horasNecesarias() }
     fun horasReuniones() =  this.totalHorasSubtareas() / 8
-    override fun horasNecesarias() = this.totalHorasSubtareas() + this.horasReuniones()
+    override fun horasNecesarias() = this.totalHorasSubtareas() + this.horasReuniones() //horasNecesarias
+
     fun totalCostoSubtareas() = listaTareas.sumBy { it.costoTarea() }
     fun porcentajeResponsable() = this.totalCostoSubtareas() * 0.03
-    override fun costoTarea() = (this.totalCostoSubtareas() + this.porcentajeResponsable()).toInt()
+    override fun costoTarea() = (this.totalCostoSubtareas() + this.porcentajeResponsable()).toInt() //costo
 
-    override fun salarioTotalEmpleados() = listaTareas.sumBy { it.salarioTotalEmpleados() } // nominaEmpleados
-    //Para la nómina de empleados, se debe incluir a las nóminas de las subtareas más al responsable de la tarea de integración.
-    //la suma de los registros financieros de los sueldos de los empleados
+    override fun nominaEmpleados(): List<Empleado> = listaTareas.flatMap{ it.nominaEmpleados() } + responsable // nominaEmpleados
+
 }
